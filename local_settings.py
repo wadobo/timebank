a2ce5c6c3709c5dba6a877b54e3edb604deca53b
Eduardo Robles Elvira <edulix@gmail.com>
2010-10-09 10:36:24 +0200

    updating local settings with new mail backend

--- local_settings.py
+++ local_settings.py
@@ -1,4 +1,10 @@
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = '1025'
 
+## For debugging, you can use this:
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = '1025'
+#
+# And you'll get the emails in the console executing:
 # python -m smtpd -n -c DebuggingServer localhost:1025
+
+# Or you can use the console email backend if you are using django >= 1.2:
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
