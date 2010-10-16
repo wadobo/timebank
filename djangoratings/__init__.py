fe232b3e312cc2c4902d7f8cdc2a429dc9961e07
Eduardo Robles Elvira <edulix@gmail.com>
2010-10-16 20:16:02 +0200

    adding djangoratings. not being used yet though!

---
+++ djangoratings/__init__.py
@@ -0,0 +1,46 @@
+import os.path
+import warnings
+
+__version__ = (0, 3, 5)
+
+def _get_git_revision(path):
+    revision_file = os.path.join(path, 'refs', 'heads', 'master')
+    if not os.path.exists(revision_file):
+        return None
+    fh = open(revision_file, 'r')
+    try:
+        return fh.read()
+    finally:
+        fh.close()
+
+def get_revision():
+    """
+    :returns: Revision number of this branch/checkout, if available. None if
+        no revision number can be determined.
+    """
+    package_dir = os.path.dirname(__file__)
+    checkout_dir = os.path.normpath(os.path.join(package_dir, '..'))
+    path = os.path.join(checkout_dir, '.git')
+    if os.path.exists(path):
+        return _get_git_revision(path)
+    return None
+
+__build__ = get_revision()
+
+def lazy_object(location):
+    def inner(*args, **kwargs):
+        parts = location.rsplit('.', 1)
+        warnings.warn('`djangoratings.%s` is deprecated. Please use `%s` instead.' % (parts[1], location), DeprecationWarning)
+        try:
+            imp = __import__(parts[0], globals(), locals(), [parts[1]], -1)
+        except:
+            imp = __import__(parts[0], globals(), locals(), [parts[1]])
+        func = getattr(imp, parts[1])
+        if callable(func):
+            return func(*args, **kwargs)
+        return func
+    return inner
+
+RatingField = lazy_object('djangoratings.fields.RatingField')
+AnonymousRatingField = lazy_object('djangoratings.fields.AnonymousRatingField')
+Rating = lazy_object('djangoratings.fields.Rating')
---
+++ djangoratings/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+from django.contrib.auth.models import User
+
+import datetime
+
+from managers import VoteManager, SimilarUserManager
+
+class Vote(models.Model):
+    content_type    = models.ForeignKey(ContentType, related_name="votes")
+    object_id       = models.PositiveIntegerField()
+    key             = models.CharField(max_length=32)
+    score           = models.IntegerField()
+    user            = models.ForeignKey(User, blank=True, null=True, related_name="votes")
+    ip_address      = models.IPAddressField()
+    date_added      = models.DateTimeField(default=datetime.datetime.now, editable=False)
+    date_changed    = models.DateTimeField(default=datetime.datetime.now, editable=False)
+
+    objects         = VoteManager()
+
+    content_object  = generic.GenericForeignKey()
+
+    class Meta:
+        unique_together = (('content_type', 'object_id', 'key', 'user', 'ip_address'))
+
+    def __unicode__(self):
+        return u"%s voted %s on %s" % (self.user_display, self.score, self.content_object)
+
+    def save(self, *args, **kwargs):
+        self.date_changed = datetime.datetime.now()
+        super(Vote, self).save(*args, **kwargs)
+
+    def user_display(self):
+        if self.user:
+            return "%s (%s)" % (self.user.username, self.ip_address)
+        return self.ip_address
+    user_display = property(user_display)
+
+    def partial_ip_address(self):
+        ip = self.ip_address.split('.')
+        ip[-1] = 'xxx'
+        return '.'.join(ip)
+    partial_ip_address = property(partial_ip_address)
+
+class Score(models.Model):
+    content_type    = models.ForeignKey(ContentType)
+    object_id       = models.PositiveIntegerField()
+    key             = models.CharField(max_length=32)
+    score           = models.IntegerField()
+    votes           = models.PositiveIntegerField()
+    
+    content_object  = generic.GenericForeignKey()
+
+    class Meta:
+        unique_together = (('content_type', 'object_id', 'key'),)
+
+    def __unicode__(self):
+        return u"%s scored %s with %s votes" % (self.content_object, self.score, self.votes)
+
+class SimilarUser(models.Model):
+    from_user       = models.ForeignKey(User, related_name="similar_users")
+    to_user         = models.ForeignKey(User, related_name="similar_users_from")
+    agrees          = models.PositiveIntegerField(default=0)
+    disagrees       = models.PositiveIntegerField(default=0)
+    exclude         = models.BooleanField(default=False)
+    
+    objects         = SimilarUserManager()
+    
+    class Meta:
+        unique_together = (('from_user', 'to_user'),)
+
+    def __unicode__(self):
+        print u"%s %s similar to %s" % (self.from_user, self.exclude and 'is not' or 'is', self.to_user)
+
+class IgnoredObject(models.Model):
+    user            = models.ForeignKey(User)
+    content_type    = models.ForeignKey(ContentType)
+    object_id       = models.PositiveIntegerField()
+    
+    content_object  = generic.GenericForeignKey()
+    
+    class Meta:
+        unique_together = (('content_type', 'object_id'),)
+    
+    def __unicode__(self):
+        return self.content_object
