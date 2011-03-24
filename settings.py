b52058f9103d49546ada37e926073bd3c7fd60b2
danigm <danigm@wadobo.com>
2011-03-24 22:45:41 +0100

    Removed old database definition in settings

--- settings.py
+++ settings.py
@@ -37,18 +37,16 @@ DEFAULT_FROM_EMAIL="admin@localhost"
 
 OWNERS = MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'
-
-# Database name or path to database file if using sqlite3.
-DATABASE_NAME = 'database.sqlite'
-
-# Not used with sqlite3.
-DATABASE_USER = ''
-DATABASE_PASSWORD = ''
-
-# Set to empty string for localhost. Not used with sqlite3.
-DATABASE_HOST = ''
-DATABASE_PORT = ''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'database.sqlite',       # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
